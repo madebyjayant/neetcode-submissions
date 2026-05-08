f05d91f95e2fa63532c319a20823d56f45cf4d35
madebyjayant <242948022+madebyjayant@users.noreply.github.com>
2026-05-08 06:42:13 +0530

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.go
@@ -0,0 +1,15 @@
+func twoSum(numbers []int, target int) []int {
+	i,j:=0,len(numbers)-1
+	for i<j{
+		curr := numbers[i]+numbers[j]
+		if curr==target{
+			return []int{i+1,j+1}
+		}
+		if curr<target {
+			i++
+			continue;
+		}
+		j--
+	}
+	return []int{-1,-1}
+}
