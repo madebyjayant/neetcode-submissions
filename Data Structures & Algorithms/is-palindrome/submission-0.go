369307e257d63df8b114921060e15e9eb94ff095
madebyjayant <242948022+madebyjayant@users.noreply.github.com>
2026-05-08 06:34:51 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.go
@@ -0,0 +1,13 @@
+func isPalindrome(s string) bool {
+	re := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	ns := strings.ToLower(re.ReplaceAllString(s, ""))
+	j:=len(ns)-1
+	i:=0
+	for i<len(s) && j>=0 && i<=j{
+		if ns[i]!=ns[j]{
+			return false
+		}
+		i++;j--
+	}
+	return true
+}
