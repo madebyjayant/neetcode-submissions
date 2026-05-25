1e1e4870338b75d691093972a36dfeefd2749bd2
madebyjayant <242948022+madebyjayant@users.noreply.github.com>
2026-05-25 15:13:32 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.go
@@ -0,0 +1,29 @@
+func checkInclusion(s1 string, s2 string) bool {
+    if len(s1)>len(s2){
+        return false
+    }
+
+    var count1, count2 [26]int
+
+    for idx, _ := range s1 {
+        count1[s1[idx]-'a']++
+        count2[s2[idx]-'a']++
+    }
+    
+    l, r := 0,len(s1)-1
+    for l<=r {
+        fmt.Println("indexes: ", l, r)
+        if count1==count2{
+            return true
+        }
+        
+        // remove the first of the window
+        count2[s2[l]-'a']--
+        l++
+        // add the next
+        r++
+        if r>len(s2)-1 {break}
+        count2[s2[r]-'a']++
+    }
+    return false
+}
