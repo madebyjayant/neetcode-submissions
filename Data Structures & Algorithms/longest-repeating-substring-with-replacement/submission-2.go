3eb21dd55c9519b054d7565031193ebf8c2ac279
madebyjayant <242948022+madebyjayant@users.noreply.github.com>
2026-05-25 12:01:56 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.go
@@ -0,0 +1,23 @@
+func characterReplacement(s string, k int) int {
+    left := 0
+    best := 0
+    seen := make(map[byte]int, 26)
+    freq := 0
+    right := 0
+    for right = 0;right < len(s); right++ {
+        seen[s[right]]++
+        if freq < seen[s[right]]{
+            // this means the current char has become more frequent
+            freq = seen[s[right]]
+        }
+        windowSize := right-left+1
+        if k >= windowSize-freq{
+            best = max(best, windowSize)
+        }else{
+            left++
+            right=left-1
+            seen = make(map[byte]int, 26)
+        }
+    }
+    return best
+}
