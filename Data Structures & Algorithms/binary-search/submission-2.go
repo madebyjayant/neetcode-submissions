251ba3b798b18ddc639f38d8cd1a756472a5f388
madebyjayant <242948022+madebyjayant@users.noreply.github.com>
2026-06-09 06:02:21 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,19 @@
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := (l+r)/2
+		
+		if nums[m]==target{
+			return m
+		}
+		if nums[m]<target{
+			l=m+1
+			continue
+		}
+		if nums[m]>target{
+			r=m-1
+			continue
+		}
+	}
+	return -1
+}
